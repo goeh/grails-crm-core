cdf06e691c0a54db74abe928aa2152bed0eda32c
Goran Ehrsson <goran@technipelago.se>
2013-09-12 22:38:08 +0200

    1.2.1

--- CrmCoreGrailsPlugin.groovy
+++ CrmCoreGrailsPlugin.groovy
@@ -22,7 +22,7 @@ import grails.plugins.crm.core.ApplicationContextHolder
  */
 class CrmCoreGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.1-SNAPSHOT"
+    def version = "1.2.1"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['controllers']
