5ed8c13301791e8ea2e788bfded66f8bc15fac23
Goran Ehrsson <goran@technipelago.se>
2014-07-15 21:35:25 +0200

    Updated copyright

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.work.dir = "target"
 grails.project.target.level = 1.6
-
-grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
     inherits("global") {}
@@ -38,33 +35,3 @@ grails.project.dependency.resolution = {
         compile(":platform-core:1.0.0") { excludes 'resources' }
     }
 }
-
-codenarc {
-    reports = {
-        CrmXmlReport('xml') {
-            outputFile = 'target/CodeNarcReport.xml'
-            title = 'GR8 CRM CodeNarc Report'
-        }
-        CrmHtmlReport('html') {
-            outputFile = 'target/CodeNarcReport.html'
-            title = 'GR8 CRM CodeNarc Report'
-
-        }
-    }
-    properties = {
-        GrailsPublicControllerMethod.enabled = false
-        CatchException.enabled = false
-        CatchThrowable.enabled = false
-        ThrowException.enabled = false
-        ThrowRuntimeException.enabled = false
-        GrailsStatelessService.enabled = false
-        GrailsStatelessService.ignoreFieldNames = "dataSource,scope,sessionFactory,transactional,*Service,messageSource,grailsApplication,applicationContext,expose"
-    }
-    processTestUnit = false
-    processTestIntegration = false
-}
-
-coverage {
-    exclusions = ['**/radar/**']
-}
-
