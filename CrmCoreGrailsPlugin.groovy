33a2ea170bac69f2113d7874137d87f1f83bb5a4
Goran Ehrsson <goran@technipelago.se>
2015-03-09 23:36:43 +0100

    Version 2.4.0

--- CrmCoreGrailsPlugin.groovy
+++ CrmCoreGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import grails.plugins.crm.core.TenantUtils
  */
 class CrmCoreGrailsPlugin {
     def groupId = ""
-    def version = "2.4.0-SNAPSHOT"
+    def version = "2.4.0"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['controllers']
